235c60f97dead6563a95bac6464b2b56c504d535
aa-tan <a.tan@keio.jp>
2017-08-18 16:03:09 -0400

    Changed Sample data

---
+++ csv.py
@@ -0,0 +1,62 @@
+import json
+import re
+import dateutil.parser as dp
+import datetime as dt
+import setup
+
+
+def getStartTime(dataPath, filePath):  # gets start time of data
+    openFile = "{}{}.LBL".format(dataPath, getFileName(filePath))
+    with open(openFile) as f:
+        for line in f:
+            startTime = re.search("START_TIME += (.+)", line)
+            if startTime != None:
+                return startTime.group(1)
+
+
+def toEpoch(iso):  # converts ISO8106 to UNIX Epoch time
+    parsed = dp.parse(iso)
+    return parsed.strftime("%s")
+
+
+def toIso(epoch):  # converts UNIX Epoch to ISO8106 time
+    return dt.datetime.utcfromtimestamp(float(epoch)).isoformat()
+
+
+def writeHeader(writeLoc, filePath):  # creates empty csv file and writes header info
+    header = "t_utc,timestamp,pressure,temp_250,temp_500,temp_1000,temp_absolute\n"
+    outFileName = "{}{}.csv".format(writeLoc, getFileName(filePath))
+    with open(outFileName, "w") as f:
+        f.write(header)
+
+
+def getFileName(filePath):  # returns only filename, used for appending extension
+    fileName = re.search('.+\/(.+)\....', filePath)
+    return fileName.group(1)
+
+
+def writeData(params, filePath):  # main function for writing CSV files
+    writeHeader(params["write_location"], filePath)
+    outString = ""
+    tempString = ""
+    timeStart = getStartTime(params["data_path"], filePath)
+    epoch = toEpoch(timeStart)
+    outFileName = "{}{}.csv".format(params["write_location"], getFileName(filePath))
+    with open(filePath) as f:
+        for line in f:
+            tempString = line.split(",")
+            tempEpoch = float(epoch) + float(tempString[0])
+            tempString.pop(0)
+            tempIso = toIso(tempEpoch)
+            tempString.insert(0, str(tempIso))
+            tempString.insert(0, str(tempEpoch))
+            outString += ",".join(tempString)
+    with open(outFileName, "a") as f:
+        f.write(outString)
+
+if __name__ == '__main__':
+    # executing will create csv files for file defined in parameters.json
+    # creates only for temperature and pressure
+    params = setup.loadParams()
+    for count in range(len(params["file_names"]["tabs"])):
+        writeData(params, params["file_names"]["tabs"][count])
--- mine.py
+++ mine.py
@@ -1,8 +1,1 @@
-import setup
-
-
-if __name__ == '__main__':
-    dataPath = "./SampleData/"
-    fileList = setup.getAllFiles(dataPath)
-    for item in fileList:
-        print(item)
+# unused
