1f95306c36468964e5c14663d47e0b6a0368344c
aa-tan <a.tan@keio.jp>
2017-08-22 08:43:53 -0400

    removed test directory

--- test/test.py
+++
@@ -1,9 +0,0 @@
-def lineCount():
-    with open("./TELLTALE_01_30.TAB") as fileName:
-        x = 0
-        for line in fileName:
-            x += 1
-        print(x)
-
-if __name__ == '__main__':
-    lineCount()
