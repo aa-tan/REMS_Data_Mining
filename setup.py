7b80907c9e3557d5843e372bef5a8b7a3db00efc
aa-tan <a.tan@keio.jp>
2017-08-24 10:17:59 -0400

    Defined getWindFiles method

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import re
 def loadParams(dataSet):  # reads parameters file
     params = {}
     with open("./parameters.json") as f:
-
         params = json.load(f)
     if dataSet == "temp":
         params = params["temperature"]
@@ -14,15 +13,28 @@ def loadParams(dataSet):  # reads parameters file
         print("loading temp")
         return params
     elif dataSet == "wind":
-        # params = getFiles
-        # return params["wind"]
-        pass
+        params = params["wind"]
+        params = getWindFiles(params)
+        print("loading wind")
+        return params
 
 
 def getTempFiles(params):  # populates params dictionary with filepaths as defined in parameters.json
     for path, suDirs, files in os.walk(params["data_path"]):
         for name in files:
             match = re.search("(.+RMC_.+(...))", name)
+            if match != None:
+                if match.group(2) == "LBL":
+                    params["file_names"]["lbls"].append(os.path.join(path, name))
+                elif match.group(2) == "TAB":
+                    params["file_names"]["tabs"].append(os.path.join(path, name))
+    return params
+
+
+def getWindFiles(params):
+    for path, suDirs, files in os.walk(params["data_path"]):
+        for name in files:
+            match = re.search("(TELLTALE.+\.(...))", name)
             if match != None:
                 if match.group(2) == "LBL":
                     params["file_names"]["lbls"].append(os.path.join(path, name))
