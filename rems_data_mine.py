fb256c7061ba154f3065214acd399bce66ee5316
aa-tan <a.tan@keio.jp>
2017-09-11 11:35:52 -0400

    Fixed arguments and prettified json

--- rems_data_mine.py
+++ rems_data_mine.py
@@ -13,11 +13,10 @@ def load_properties():
     properties = {}
     with open("./properties.json") as f:
         properties = json.load(f)
-        properties = get_files(properties)
         return properties
 
 
-def get_files(properties):
+def get_files():
     # populates properties dictionary with filepaths as defined in properties.json
     for path, suDirs, files in os.walk(properties["data_path"]):
         for name in files:
@@ -29,7 +28,6 @@ def get_files(properties):
                 elif match.group(1) == "TAB":
                     properties["file_names"]["tabs"].append(
                         os.path.join(path, name))
-    return properties
 
 
 def to_unix(timestamp):
@@ -74,7 +72,7 @@ def prepare_line(filePath):
 def write_header(filePath):
     # creates empty csv file and writes header info
     header = "t_utc,timestamp,h_wind_speed,v_wind_speed,wind_dir,ambient_temp,humidity,pressure\n"
-    outFileName = "{}CSV/{}.csv".format(
+    outFileName = "{}{}.csv".format(
         properties["write_location"], get_file_name(filePath))
     print("Writing header of file: {}".format(outFileName))
     try:
@@ -87,7 +85,7 @@ def write_header(filePath):
 def write_data(filePath):
     # function for writing CSV files
     outString = prepare_line(filePath)
-    outFileName = "{}CSV/{}.csv".format(
+    outFileName = "{}{}.csv".format(
         properties["write_location"], get_file_name(filePath))
     print("Writing CSV File")
     try:
@@ -101,12 +99,12 @@ def write_JSON(filePath):
     # writes JSON files defining DB action
     print("Writing JSON File")
     try:
-        out = {"action": "insert", "database": "sam.rems_temp",
+        out = {"action": "insert", "database": properties["database"],
                "records": {"type": "csv"}}
-        out["records"]["$object_id"] = "{}CSV/{}.csv".format(
+        out["records"]["$object_id"] = "{}{}.csv".format(
             properties["write_location"], get_file_name(filePath))
         with open("{}{}.json".format(properties["write_location"], get_file_name(filePath)), "w") as f:
-            json.dump(out, f)
+            json.dump(out, f, indent=4, sort_keys=True)
         print("Success\n")
     except:
         print("Failed to write JSON File")
@@ -134,7 +132,6 @@ def execute():
             write_header(filePath[count])
             write_data(filePath[count])
             write_JSON(filePath[count])
-
     except:
         print("\n\n----\nAN ERROR HAS OCCURRED\n----\\nn")
 
@@ -143,10 +140,15 @@ def execute():
 properties = load_properties()
 if __name__ == '__main__':
     # executing will create csv files for files defined in properties.json
+    # or as defined in arguments
     if len(sys.argv) == 3:
         print("Updating path to match arguments")
         update_path(sys.argv[1], sys.argv[2])
+        get_files()
+        print("{}\n{}".format(
+            properties["data_path"], properties["write_location"]))
         execute()
     elif len(sys.argv) == 1:
         print("Running using properties.json paths")
+        get_files(properties)
         execute()
